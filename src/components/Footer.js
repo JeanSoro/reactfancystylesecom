d885a942e620df1773a9839bf467dcd12951adb7
Jean <jean.soro007@gmail.com>
2020-12-11 10:27:19 -0500

    Added sidebar component visuals

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
+
+
+
 const Footer = () => {
-  return <h4>footer</h4>
+  return (
+    <Wrapper>
+      <h5>
+        &copy; {new Date().getFullYear()}
+        <span> fancy ecomm</span>
+      </h5>
+      <h5>All right reserved</h5>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.footer`
