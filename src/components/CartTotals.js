2092b3b36dca7c04d6cceced39557b5c3ea0e56d
Jean <jean.soro007@gmail.com>
2021-02-10 19:35:58 -0500

    Finish setting up jsx for cartItem component including dummy functions

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -4,6 +4,7 @@ import { useCartContext } from '../context/cart_context'
 import { useUserContext } from '../context/user_context'
 import { formatPrice } from '../utils/helpers'
 import { Link } from 'react-router-dom'
+import { format } from 'util';
 
 const CartTotals = () => {
 
@@ -15,7 +16,12 @@ const CartTotals = () => {
         <article>
           <h5>subtotal: <span>{formatPrice(total_amount)}</span></h5>
           <p>shipping fee: <span>{formatPrice(shipping_fee)}</span></p>
+          <hr />
+          <h4>
+            order total : <span>{formatPrice(total_amount + shipping_fee)}</span>
+          </h4>
         </article>
+        <Link to="/checkout" className="btn">proceed to checkout</Link>
       </div>
     </Wrapper>
   )
